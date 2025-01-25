cffaeaed1076fe63b7854c4fbbc545b192354bb2
zDragonPaulo <23919@stu.ipbeja.pt>
2025-01-25 00:28:43 +0000

    Mais Views Adicionadas

---
+++ app/src/main/java/com/example/trailsnapv2/ui/walk/WalkDetailsFragment.kt
@@ -0,0 +1,31 @@
+package com.example.trailsnapv2.ui.walk
+
+import androidx.fragment.app.viewModels
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.trailsnapv2.R
+
+class WalkDetailsFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = WalkDetailsFragment()
+    }
+
+    private val viewModel: WalkDetailsViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // TODO: Use the ViewModel
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_walk_details, container, false)
+    }
+}
---
+++ app/src/main/java/com/example/trailsnapv2/ui/walk/WalkDetailsViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.trailsnapv2.ui.walk
+
+import androidx.lifecycle.ViewModel
+
+class WalkDetailsViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
